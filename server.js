f0244759e0b38c857e3269b904ed4e1ce5010a50
Michael <michaelfoley1999@gmail.com>
2020-04-02 20:54:15 -0400

    Login and create account page

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const path = require('path')
 const app = express();
+
 
 const port = 3000;
 
 app.listen(port, () => console.log('Listening at port ' + port));
 app.use(express.static('public'));
+
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'loginForm.html'));
+});
+
+app.get('/signup', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'createAccount.html'));
+});
+
+app.get('/test', (req, res) =>{
+
+    res.send('This is TOTALLY a test');
+
+});
