127f2c0f339ab8da1d3cbf6d0a5a94dc168049af
ryomakawakami <ryomakwkm@gmail.com>
2020-04-21 12:38:31 -0400

    Added salt and hash regen script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # userAccounts
+
+Run regenerate.py from userAccounts directory (python userAccounts/regnerate.py) 
+to regenerate salts and hashes for current hashing algorithm.
---
+++ pyAuth/regenerate.py
@@ -0,0 +1,9 @@
+import os
+import hasher
+
+with open('admin/backup', 'r') as f:
+    with open('admin/new', 'w') as fo:
+        for line in f:
+            u, p = line.strip().split(' ')
+            s, h = hasher.newHash(p)
+            fo.write(u + " " + s + " " + h + "\n")
